527267f28d1c5d4c87838ffd14a570354d310cf0
Palash Sanjay Jain <palashsjain8@gmail.com>
2016-11-25 23:34:32 -0500

    Updated basic tests to make use of Pointcut class

--- test/alter.js
+++ test/alter.js
@@ -2,48 +2,71 @@
  * Created by Palash on 11/24/2016.
  */
 
-doBefore = BasicAspect.before(doBefore, function () {
-    console.log("Argument object passed to doBefore was " + doBefore.arguments);
+let pBefore = new Pointcut("doBefore", window);
+pBefore.before(function(){
+    if (pBefore.arguments().length == 0) {
+        console.log("No arguments were passed to doBefore.");
+    } else {
+        console.log("Arguments passed to doBefore was " + pBefore.arguments());
+    }
 });
 
-doAround = BasicAspect.around(doAround, function () {
-    console.log("adding before part in around advice");
-}, function () {
-    console.log("adding after part in around advice");
+let pAround = new Pointcut("doAround", window);
+pAround.around(function(){
+    console.log("doAround method started.");
+    return pAround.proceed();
 });
 
-doAfter = BasicAspect.after(doAfter, function () {
-    console.log("Argument object passed to doAfter was " + doAfter.arguments);
+let pAfter = new Pointcut("doAfter", window);
+pAfter.after(function(){
+    if (pAfter.arguments().length == 0) {
+        console.log("No arguments were passed to doAfter.");
+    } else {
+        console.log("Arguments passed to doBefore was " + pAfter.arguments());
+    }
 });
 
-Calculator.prototype.divide = BasicAspect.before(Calculator.prototype.divide, function () {
-    var digits = Array.from(Calculator.prototype.divide.arguments);
+let pStackTrace = new Pointcut("demoStackTrace", window);
+pStackTrace.before(function(){
+    console.log("demoStackTrace was called from " + pStackTrace.pointcut().caller.name);
+    console.log(pStackTrace.pointcut().caller.name + " was called from " + pStackTrace.pointcut().caller.caller.name);
+    console.log("Arguments passed to " + pStackTrace.pointcut().caller.name + " function were " + Array.from(pStackTrace.pointcut().caller.arguments));
+    console.log("Arguments passed to " + pStackTrace.pointcut().caller.caller.name + " function were " + Array.from(pStackTrace.pointcut().caller.caller.arguments));
+});
+// demoStackTrace = Pointcut.before(demoStackTrace, function () {
+
+// });
+
+let pMultiply = new Pointcut("multiply", Calculator.prototype);
+pMultiply.around(function(){
+    console.log("AROUND STARTED.");
+    let temp = pMultiply.proceed();
+    console.log("the output was " + temp);
+    console.log("AROUND ENDED.");
+    return temp;
+});
+
+let pDivide = new Pointcut("divide", Calculator.prototype);
+pDivide.before(function () {
+    console.log("BEFORE STARTED.");
+    let digits = pDivide.arguments();
     if (digits && digits.length == 2){
         console.log("Divide " + digits[0] + " from " + digits[1]);
         if (digits[1] == 0)
-            throw new BasicAspectException("fatal", "Cannot divide by 0.");
+            throw {
+                message : "Cannot divide by 0."
+            };
     }
+    console.log("BEFORE ENDED.");
 });
 
-Calculator.prototype.multiply = BasicAspect.around(Calculator.prototype.multiply, function () {
-    var digits = Array.from(Calculator.prototype.multiply.arguments);
-    if (digits && digits.length == 2) {
-        console.log("Multiply " + digits[0] + " with " + digits[1]);
-    }
-}, function (product) {
-    console.log("The product is " + product);
-});
-
-Calculator.prototype.subtract = BasicAspect.after(Calculator.prototype.subtract, function (temp) {
-    var digits = Array.from(Calculator.prototype.subtract.arguments);
+let pSubtract = new Pointcut("subtract", Calculator.prototype);
+pSubtract.after(function(output){
+    console.log("AFTER STARTED.")
+    let digits = pSubtract.arguments();
     if (digits && digits.length == 2){
-        console.log("Subtracting " + digits[0] + " from " + digits[1] + " gives " + temp);
+        console.log(pSubtract.proceed());
+        console.log("Subtracting " + digits[0] + " from " + digits[1] + " gives " + output);
     }
-});
-
-demoStackTrace = BasicAspect.before(demoStackTrace, function () {
-    console.log("demoStackTrace was called from " + demoStackTrace.caller.name);
-    console.log(demoStackTrace.caller.name + " was called from " + demoStackTrace.caller.caller.name);
-    console.log("Arguments passed to " + demoStackTrace.caller.name + " function were " + Array.from(demoStackTrace.caller.arguments));
-    console.log("Arguments passed to " + demoStackTrace.caller.caller.name + " function were " + Array.from(demoStackTrace.caller.caller.arguments));
+    console.log("AFTER ENDED.")
 });
--- test/main.js
+++ test/main.js
@@ -3,29 +3,32 @@
  */
 
 function doBefore(num) {
-    console.log("doing stuff before " + num);
+    console.log("doBefore method called from the main script.");
 }
 
 function doAround(){
-    console.log("doing stuff around");
+    console.log("doAround method called from the main script.");
     return 3;
 }
 
 function doAfter(){
-    console.log("doing stuff after");
+    console.log("doAfter method called from the main script.");
 }
 
 var Calculator = {};
 Calculator.prototype = {
     multiply: function (x, y) {
+        console.log("Multiplying method called from the main script.");
         return x * y;
     },
 
     subtract: function (x, y) {
+        console.log("Subtraction method called from the main script.");
         return x - y;
     },
 
     divide: function (x, y) {
+        console.log("Division method called from the main script.");
         return x / y;
     },
 };
