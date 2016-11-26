3ae55bc414f1e2c2b0fcd0a048b58afa70aef126
Palash Sanjay Jain <palashsjain8@gmail.com>
2016-11-25 23:35:22 -0500

    Replaced Fibonacci calculation (using AOP) with actual use of around advice from Pointcut class

--- test-fibonacci/alter.js
+++ test-fibonacci/alter.js
@@ -2,14 +2,13 @@
  * Created by Palash on 11/25/2016.
  */
 var fib = [];
-var orig = calculateFibWithAOP;
-
-calculateFibWithAOP = function(){
-    var pos = Array.from(arguments);
+let pFibonacci = new Pointcut("calculateFibWithAOP", window);
+pFibonacci.around(function(){
+    var pos = pFibonacci.arguments();
     if (fib && fib[pos]){
         return fib[pos];
     } else {
-        fib[pos] = orig.apply(this, arguments);
+        fib[pos] = pFibonacci.proceed();
     }
     return fib[pos];
-}
+});
