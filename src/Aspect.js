fec9267deb5d518e2e3272949530382a80aeabe9
Palash Sanjay Jain <palashsjain8@gmail.com>
2016-11-27 20:29:11 -0500

    Changed Pointcut from Class to function prototype

--- src/Aspect.js
+++ src/Aspect.js
@@ -2,20 +2,38 @@
  * Created by Palash on 11/24/2016.
  */
 
-function before(pointcut, advice){
-    return function () {
+var Pointcut = function (exp, context) {
+    this.exp = exp;
+    this.context = context;
+    this.orig = context[exp];
+};
+
+Pointcut.prototype.pointcut = function () {
+    return this.context[this.exp];
+};
+
+Pointcut.prototype.arguments = function () {
+    return Array.from(this.pointcut().arguments);
+};
+
+Pointcut.prototype.before = function (advice) {
+    this.insideAround = false;
+    let p = this.context[this.exp];
+    this.context[this.exp] = function () {
         try {
             advice();
         } catch (e) {
             console.log("Exception thrown from before advice: " + e.message);
         }
-        return pointcut.apply(this, arguments);
-    }
-}
+        return p.apply(this, arguments);
+    };
+};
 
-function after(pointcut, advice){
-    return function () {
-        let temp = pointcut.apply(this, arguments);
+Pointcut.prototype.after = function (advice) {
+    this.insideAround = false;
+    let p = this.context[this.exp];
+    this.context[this.exp] = function () {
+        let temp = p.apply(this, arguments);
         try {
             advice(temp);
         } catch (e) {
@@ -24,8 +42,49 @@ function after(pointcut, advice){
         }
         return temp;
     }
-}
+};
 
+Pointcut.prototype.around = function (advice) {
+    this.insideAround = true;
+    this.context[this.exp] = advice;
+};
+
+Pointcut.prototype.proceed = function () {
+    if (this.insideAround) {
+        return this.orig.apply(this, this.arguments());
+    }
+    throw {
+        message: "Proceed can only be called from around advice."
+    }
+};
+
+/*
+ function before(pointcut, advice){
+ return function () {
+ try {
+ advice();
+ } catch (e) {
+ console.log("Exception thrown from before advice: " + e.message);
+ }
+ return pointcut.apply(this, arguments);
+ }
+ }
+
+ function after(pointcut, advice){
+ return function () {
+ let temp = pointcut.apply(this, arguments);
+ try {
+ advice(temp);
+ } catch (e) {
+ console.log("Exception thrown from after advice: " + e.message);
+ throw e;
+ }
+ return temp;
+ }
+ }
+ */
+
+/*
 class Pointcut {
     constructor(exp, context) {
         this.exp = exp;
@@ -64,5 +123,4 @@ class Pointcut {
             message: "Proceed can only be called from around advice."
         }
     }
-
-}
+}*/
